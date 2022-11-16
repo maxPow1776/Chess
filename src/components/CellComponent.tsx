f3d5289f0b5bc5bba242e943d5eba396ac52324a
maxPow1776 <maks.greb2@gmail.com>
2022-11-16 17:20:55 +0600

    added figures to the board

--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -7,7 +7,9 @@ interface CellProps {
 
 const CellComponent: FC<CellProps> = ({cell}) => {
   return (
-    <div className={['cell', cell.color].join(' ')} />
+    <div className={['cell', cell.color].join(' ')}>
+      {cell.figure?.logo && <img src={cell.figure.logo} alt=""/>}
+    </div>
   )
 }
 
